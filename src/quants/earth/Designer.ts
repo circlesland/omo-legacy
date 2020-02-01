e9985b65da666f4570697cf9b52ae7ae14d658ec
Samuel Andert <samuelandert@me.com>
2020-02-01 23:42:22 +0100

    added basic tailwind ui design

--- src/quants/earth/Designer.ts
+++ src/quants/earth/Designer.ts
@@ -1,13 +1,14 @@
-import DragableQuant from "../../kernel/quants/DragableQuant";
+import DragableQuant from '../../kernel/quants/DragableQuant';
 
 export default class Designer extends DragableQuant {
-    private quantaList: any;
-    private contextSwitch: any;
-    constructor() {
-        super();
-    }
-    public render(): void {
-        return omo.html`
+  private quantaList: any;
+  private contextSwitch: any;
+  constructor() {
+    super();
+  }
+  public render(): void {
+    return omo.html`
+    
         <omo-earth-quantaList></omo-earth-quantaList>
         <omo-earth-contextSwitch></omo-earth-contextSwitch>
         <omo-earth-viewsChooser></omo-earth-viewsChooser>
@@ -15,21 +16,27 @@ export default class Designer extends DragableQuant {
         <omo-earth-actions></omo-earth-actions>
         <omo-earth-versions></omo-earth-versions>
         <omo-earth-splitView></omo-earth-splitView>
+
         `;
-    }
-    public firstUpdated(changedProperties: any): void {
-        super.firstUpdated(changedProperties);
-        this.quantaList = this.root.querySelector("omo-earth-quantalist");
-        this.contextSwitch = this.root.querySelector("omo-earth-contextswitch");
-        this.quantaList.addEventListener("selectedQuant", () => this.setSelectedQuant(this.quantaList.selectedQuant), false);
-    }
+  }
+  public firstUpdated(changedProperties: any): void {
+    super.firstUpdated(changedProperties);
+    this.quantaList = this.root.querySelector('omo-earth-quantalist');
+    this.contextSwitch = this.root.querySelector('omo-earth-contextswitch');
+    this.quantaList.addEventListener(
+      'selectedQuant',
+      () => this.setSelectedQuant(this.quantaList.selectedQuant),
+      false
+    );
+  }
 
-    setSelectedQuant(selectedQuant: any) {
-        this.contextSwitch.selectedQuant = selectedQuant;
-    }
+  setSelectedQuant(selectedQuant: any) {
+    this.contextSwitch.selectedQuant = selectedQuant;
+  }
 
-    public static get styles(): any {
-        return [omo.css/*css*/`
+  public static get styles(): any {
+    return [
+      omo.css/*css*/ `
     :host{
         height:100%;
         display:grid;
@@ -38,9 +45,8 @@ export default class Designer extends DragableQuant {
         "quanta split views"
         "data split versions"
         "data actions versions";
-
-        grid-template-colums: auto 4fr auto;
-        grid-template-rows:auto 1fr 1fr auto;
+        grid-template-columns: 16rem auto 16rem;
+        grid-template-rows: auto auto 1fr auto;
     }
     omo-earth-quantaList {grid-area:quanta;}
     omo-earth-contextSwitch {grid-area:nav;}
@@ -49,13 +55,13 @@ export default class Designer extends DragableQuant {
     omo-earth-actions {grid-area:actions;}
     omo-earth-versions {grid-area:versions;}
     omo-earth-splitView {grid-area:split;}
-    `]
-    }
-    static get properties(): any {
-        return super.properties;
-    }
-    static get model(): any {
-        return {
-        };
-    }
+    `
+    ];
+  }
+  static get properties(): any {
+    return super.properties;
+  }
+  static get model(): any {
+    return {};
+  }
 }
--- src/quants/earth/QuantaList.ts
+++ src/quants/earth/QuantaList.ts
@@ -1,54 +1,87 @@
-import DragableQuant from "../../kernel/quants/DragableQuant";
+import DragableQuant from '../../kernel/quants/DragableQuant';
 
 export default class QuantaList extends DragableQuant {
-    static get properties(): any {
-        return super.properties;
-    }
-    static get model(): any {
-        return {
-            quanta: {
-                type: "array"
-            },
-            selectedQuant: {
-                type: "string"
-            }
-        };
-    }
-    public quanta: any[];
-    public selectedQuant: any;
-    constructor() {
-        super();
-        this.autosave = false;
-        this.quanta = [];
-        this.listenToQuanta();
+  static get properties(): any {
+    return super.properties;
+  }
+  static get model(): any {
+    return {
+      quanta: {
+        type: 'array'
+      },
+      selectedQuant: {
+        type: 'string'
+      }
+    };
+  }
+  public quanta: any[];
+  public selectedQuant: any;
+  constructor() {
+    super();
+    this.autosave = false;
+    this.quanta = [];
+    this.listenToQuanta();
+  }
+  public async listenToQuanta(): Promise<void> {
+    omo.client.listen(
+      omo.quantum.QuantStoreId,
+      await omo.quantum.QuantModelName(),
+      '',
+      this.updateQuanta
+    );
+    this.updateQuanta();
+  }
+  public async updateQuanta(): Promise<void> {
+    this.quanta = (
+      await omo.client.modelFind(
+        omo.quantum.QuantStoreId,
+        await omo.quantum.QuantModelName(),
+        {}
+      )
+    ).entitiesList;
+  }
+  public render(): void {
+    return omo.html`
+        <div class="h-full px-8 py-6 bg-gray-200 w-1/5">
+            <p class="uppercase text-gray-600 text-xs font-semibold">Quanta</p>
+            <ul class="">
+                ${this.quanta.map((quant: any) => {
+                  const quantName = omo.quantum.getQuantName(
+                    quant.author,
+                    quant.project,
+                    quant.name,
+                    quant.major,
+                    quant.minor,
+                    quant.patch
+                  );
+                  return omo.html`         
+                    <li @click="${this.selectQuant}" class="px-2 py-1 font-semibold text-base hover:bg-primary hover:text-white leading-tight truncate">${quantName}</li>
+                `;
+                })}
+            </ul>
+        </div>
+        `;
+  }
 
-    }
-    public async listenToQuanta(): Promise<void> {
-        omo.client.listen(omo.quantum.QuantStoreId, await omo.quantum.QuantModelName(), "", this.updateQuanta);
-        this.updateQuanta();
-    }
-    public async updateQuanta(): Promise<void> {
-        this.quanta = (await omo.client.modelFind(omo.quantum.QuantStoreId, await omo.quantum.QuantModelName(), {})).entitiesList;
-    }
-    public render(): void {
-        return omo.html`
-        ${this.quanta.map((quant: any) => {
-            const quantName = omo.quantum.getQuantName(quant.author, quant.project, quant.name, quant.major, quant.minor,
-                quant.patch);
-            return omo.html`<a @click="${this.selectQuant}">${quantName}</a>`
-        })}`
-    }
+  public async initAsync(): Promise<void> {
+    super.initAsync();
+  }
+  public selectQuant(event: Event): void {
+    this.selectedQuant = event['path'][0].innerText;
+  }
 
-    public async initAsync(): Promise<void> {
-        super.initAsync();
-    }
-    public selectQuant(event: Event): void {
-        this.selectedQuant = event["path"][0].innerText;
-    }
+  public updated(changedProperties: any): void {
+    super.updated(changedProperties);
+  }
 
-    public updated(changedProperties: any): void {
-        super.updated(changedProperties);
-    }
-
-    static get styles() { return [omo.css`:host{ display: flex; flex-direction: column; } `] }
+  static get styles() {
+    return [
+      omo.theme,
+      omo.css`
+        :host {
+            position:relative;
+        }
+        `
+    ];
+  }
 }
