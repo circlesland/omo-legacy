3c7a03d81e0dcfafc36c22580f6bad4dca93bb15
Samuel Andert <samuelandert@me.com>
2020-04-14 22:24:29 +0200

    minor

--- src/quanta/1-views/2-molecules/OmoBanner.svelte
+++ src/quanta/1-views/2-molecules/OmoBanner.svelte
@@ -14,9 +14,11 @@
   class="bg-cover bg-center rounded h-auto text-white py-24 px-16 object-fill"
   style="background-image: url({quant.data.image})">
   <div class="md:w-1/2">
-    <p class="font-bold text-sm uppercase">{quant.data.uptitle}</p>
-    <p class="text-3xl font-bold">{quant.data.title}</p>
-    <p class="text-2xl mb-10 leading-none">{quant.data.subtitle}</p>
+    <p class="text-gray-300 text-xs uppercase">{quant.data.uptitle}</p>
+    <p class="text-3xl font-bold font-title uppercase">{quant.data.title}</p>
+    <p class="text-xl text-gray-200 mb-10 leading-none">
+      {quant.data.subtitle}
+    </p>
     <a
       href="/"
       class="bg-green-400 py-3 px-6 text-white font-bold uppercase text-xs
