0533e0f107b498b7daf3253caa5b4a7739d555f7
Philipp Bartels <philippbartels@anjas-mbp.localdomain>
2020-04-28 12:13:32 +0200

    added subscribe

--- src/quanta/Test.svelte
+++ src/quanta/Test.svelte
@@ -1,5 +1,11 @@
 <script>
-
+  window.subscribe("subscription {books {title}}").then(subscription => {
+    (async () => {
+      for await (let value of subscription) {
+        console.log("ASYNC Value", value);
+      }
+    })();
+  });
 </script>
 
 <div class="min-w-100">Test</div>
