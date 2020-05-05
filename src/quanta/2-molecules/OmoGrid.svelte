b267a25bd5934ce196736697a4442acc54d3c205
Samuel Andert <samuelandert@me.com>
2020-05-05 20:13:43 +0200

    minor grid udpate

--- src/quanta/2-molecules/OmoGrid.svelte
+++ src/quanta/2-molecules/OmoGrid.svelte
@@ -1,7 +1,7 @@
 <style>
   .omo-grid {
     display: grid;
-    grid-template-columns: 200px 150px repeat(6, 300px);
+    grid-template-columns: 200px 150px 100px repeat(6, 300px);
   }
 </style>
 
@@ -9,6 +9,27 @@
 <div class="omo-grid bg-gray-200 p-1">
   <div class="omo-grid-item bg-gray-100 py-2 px-4">model key</div>
   <div class="omo-grid-item bg-gray-100 py-2 px-4">field type</div>
+  <div class="omo-grid-item bg-white-100">
+    <button class="mx-2 py-2 px-4 bg-green-400 font-bold rounded">
+      action
+    </button>
+  </div>
+  <div class="omo-grid-item bg-white py-2 px-4 mx-1">DB value 1</div>
+  <div class="omo-grid-item bg-white py-2 px-4">DB value 2</div>
+  <div class="omo-grid-item bg-white py-2 px-4">DB value 3</div>
+  <div class="omo-grid-item bg-white py-2 px-4">DB value 4</div>
+  <div class="omo-grid-item bg-white py-2 px-4">DB value 5</div>
+  <div class="omo-grid-item bg-white py-2 px-4">DB value 6</div>
+</div>
+
+<div class="omo-grid bg-gray-200 p-1 -mt-1">
+  <div class="omo-grid-item bg-gray-100 py-2 px-4">model key</div>
+  <div class="omo-grid-item bg-gray-100 py-2 px-4">field type</div>
+  <div class="omo-grid-item bg-white-100">
+    <button class="mx-2 py-2 px-4 bg-green-400 font-bold rounded">
+      action
+    </button>
+  </div>
   <div class="omo-grid-item bg-white py-2 px-4 mx-1">DB value 1</div>
   <div class="omo-grid-item bg-white py-2 px-4">DB value 2</div>
   <div class="omo-grid-item bg-white py-2 px-4">DB value 3</div>
