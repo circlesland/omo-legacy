8134a64bae2235f538eae7bba27f8cce752b8e15
Samuel Andert <samuelandert@me.com>
2020-05-06 14:42:31 +0200

    updated the quant grid css styles

--- src/quanta/4-layouts/OmoSideBarLayout.svelte
+++ src/quanta/4-layouts/OmoSideBarLayout.svelte
@@ -4,26 +4,21 @@
   import OmoList from "./../2-molecules/OmoList.svelte";
   import OmoNavbar from "./../2-molecules/OmoNavbar.svelte";
   import OmoMenuVertical from "./../2-molecules/OmoMenuVertical.svelte";
-  import OmoGrid from "./../2-molecules/OmoGrid.svelte";
 
   import { curRoute } from "./../../router.ts";
 
   export let router;
 </script>
 
-<div class="h-full border-r border-gray-200">
+<!-- <div class="h-full border-r border-gray-200">
   <OmoMenuVertical />
-</div>
+</div> -->
 <div
   class="h-full overflow-y-scroll bg-gray-100 border-t border-r border-gray-200
   w-64">
   <OmoList routes={router} />
 </div>
-<div class="h-full flex-1 overflow-y-scroll py-20 px-24">
+<div class="h-full flex-1 overflow-y-scroll overflow-x-scroll py-20 px-24">
   <OmoQuantGrid quantId={router.find(x => x.route == $curRoute).ID} />
-  <div class="py-10">
-    <OmoGrid />
-  </div>
-  <OmoQuantTable quantId={router.find(x => x.route == $curRoute).ID} />
-
+  <!-- <OmoQuantTable quantId={router.find(x => x.route == $curRoute).ID} /> -->
 </div>
