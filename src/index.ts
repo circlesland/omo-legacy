066e3b1dd5eda8fffca84cc25ba59daba26fac73
philapp <philippbartels@outlook.com>
2020-06-18 07:24:08 +0200

    Temporary disable textile

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "./styles/index.scss";
 import App from "./App.svelte";
-import {
+/*import {
   graphql,
   printSchema,
   subscribe,
@@ -9,26 +9,26 @@ import {
 } from "graphql";
 import { getSchema } from "./schemas";
 import { initDB, db } from "./textileThreads";
-const escapeStringRegexp = require('escape-string-regexp');
+const escapeStringRegexp = require('escape-string-regexp');*/
 
-let pluralize = require('pluralize');
+// let pluralize = require('pluralize');
 let app;
-window['printSchema'] = printSchema;
-initDB(true).then(async () => {
-  window["db"] = db;
-  window["escape"] = escapeStringRegexp;
-  window["pluralize"] = pluralize;
-  window["graphql"] = async (query) => graphql(await getSchema(), query);
-  window["subscribe"] = async (query) =>
-    (await subscribe({
-      schema: await getSchema(),
-      document: parse(query),
-      rootValue: "data",
-    })) as AsyncIterableIterator<ExecutionResult>;
-  window["schema"] = async () => printSchema(await getSchema());
+// window['printSchema'] = printSchema;
+// initDB(true).then(async () => {
+//   window["db"] = db;
+//   window["escape"] = escapeStringRegexp;
+//   window["pluralize"] = pluralize;
+//   window["graphql"] = async (query) => graphql(await getSchema(), query);
+//   window["subscribe"] = async (query) =>
+//     (await subscribe({
+//       schema: await getSchema(),
+//       document: parse(query),
+//       rootValue: "data",
+//     })) as AsyncIterableIterator<ExecutionResult>;
+//   window["schema"] = async () => printSchema(await getSchema());
   app = new App({
     target: document.body,
   });
-});
+// });
 
 export default app;
