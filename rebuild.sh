a9b74dc9ff1da03c6368726ee27acf471ae7723d
jaensen <no@email.comm>
2020-08-17 13:28:40 +0200

    used "npx tsc" instead global "tsc"

--- rebuild.sh
+++ rebuild.sh
@@ -3,35 +3,35 @@ cd textile-adapter || exit
 rm -r -f dist
 rm -f package-lock.json
 npm i
-tsc
+npx tsc
 
 cd ../textile-graphql || exit
 rm -r -f dist
 rm -f package-lock.json
 npm i
-tsc
+npx tsc
 
 cd ../events || exit
 rm -r -f dist
 rm -f package-lock.json
 npm i
-tsc
+npx tsc
 
 cd ../identity || exit
 rm -r -f dist
 rm -f package-lock.json
 npm i
-tsc
+npx tsc
 
 cd ../circles || exit
 rm -r -f dist
 rm -f package-lock.json
 npm i
-tsc
+npx tsc
 
 cd ../app || exit
 rm -r -f dist
 rm -r -f node_modules/@omo
 rm -f package-lock.json
 npm i
-tsc
+npx tsc
