ff6671a38abf0f7bbed307e4d417e50fe24502e4
Samuel Andert <samuelandert@me.com>
2020-08-19 12:55:29 +0200

    save ui elements

---
+++ app/src/designer/quanta/1-views/2-molecules/OmoHero.svelte
@@ -0,0 +1,54 @@
+<script>
+  export let quant = {
+    id: "",
+    model: {
+      id: "",
+      name: "",
+      image: "",
+      author: "",
+      type: ""
+    },
+    data: {
+      id: "",
+      title: "",
+      subline: "",
+      description: "",
+      illustration: "",
+      button: ""
+    },
+    design: {
+      layout: ""
+    }
+  };
+</script>
+
+<div class="flex flex-col mx-auto {quant.design.layout}">
+  <div class="flex flex-col-reverse sm:flex-row jusitfy-between items-center">
+    <div
+      class="sm:w-3/5 flex flex-col items-center sm:items-start text-center
+      sm:text-left">
+      <h1
+        class="uppercase text-5xl text-blue-900 font-bold font-title
+        leading-none tracking-tight mb-2">
+        {quant.data.title}
+      </h1>
+      <div
+        class="text-3xl text-gray-600 font-sans italic font-light tracking-wide
+        mb-6">
+        {quant.data.subline}
+      </div>
+      <p class="text-gray-500 leading-relaxed mb-12">
+        {quant.data.description}
+      </p>
+      <a
+        href="/"
+        class="bg-green-400 hover:bg-blue-800 py-2 px-4 text-lg font-bold
+        text-white rounded">
+        {quant.data.button}
+      </a>
+    </div>
+    <div class="lg:px-20 w-2/5">
+      <img src={quant.data.illustration} alt={quant.data.title} />
+    </div>
+  </div>
+</div>
---
+++ app/src/designer/quanta/1-views/2-molecules/OmoQuantaItem.svelte
@@ -0,0 +1,30 @@
+<script>
+  import OmoQuantPreview from "./OmoQuantPreview.svelte";
+  export let quant;
+</script>
+
+<div class="bg-white max-w-sm rounded shadow-md border">
+  <OmoQuantPreview {quant} />
+  <div class="py-2 px-3 bg-gray-100">
+    <a
+      class="text-md font-semibold text-gray-700 font-medium "
+      href="/quant?id={quant.id}">
+      {quant.model.name}
+    </a>
+  </div>
+  <div class="pb-2 px-3 flex justify-between items-center mt-2">
+    <div class="flex items-center">
+      <img
+        src={quant.model.image}
+        class="w-10 h-10 object-cover rounded"
+        alt="avatar" />
+      <a class="text-gray-700 text-sm mx-3" href="/quant?id={quant.id}">
+        {quant.model.author}
+        <br />
+        <span class="font-light text-xs text-gray-500">
+          {quant.model.group}-{quant.model.type}s-{quant.model.tags}s
+        </span>
+      </a>
+    </div>
+  </div>
+</div>
